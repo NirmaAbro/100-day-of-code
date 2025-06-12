ec829fda9aecb3e5cbe44297db698b58cf1434d4
Nirma Abro <NirmaAbro143@gmail.com>
2025-06-12 01:47:59 -0600

    password generator project start , with shadcn ui , tailwind css

--- React-projects/app/src/App.jsx
+++ React-projects/app/src/App.jsx
@@ -1,35 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import toast from 'react-hot-toast'
+import { Input } from "@/components/ui/input"
+import { Button } from "@/components/ui/button"
 
 function App() {
-  const [count, setCount] = useState(0)
 
+  const handleCopy = () => {
+    toast.success('Operation successful!')
+  };
+
+  
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h1 className="text-3xl bg-amber-500 flex  justify-center items-center ">
+          Password Generator
+        </h1>
+        <div className="flex justify-center items-center border rounded-xl ">
+          {/* input  */}
+          <div className=" m-4 rounded-2xl border flex ">
+            {/* <input type="text" className=" px-3 py-2 outline-none " /> */}
+            <Input onChange={(e) => console.log(e.target.value)} type="text" className={" px-3 py-2 outline-none border-none focus:outline-none focus:ring-0 focus:ring-transparent "} />
+            {/* <button
+              className="bg-black text-white p-2 rounded-2xl hover:bg-blue-800 cursor-pointer "
+              onClick={handleCopy}
+            >
+              Copy
+            </button> */}
+            <Button variant="outline"  onClick={handleCopy} className={"bg-black text-white hover:bg-blue-800 rounded-xl  cursor-pointer "}> Copy</Button>
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
